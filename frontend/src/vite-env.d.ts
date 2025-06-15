e5175e1b7fb4c148d37be445ef17e37d5c4ddb15
KARUNANS2004 <s.karuna0074@gmail.com>
2025-06-15 22:05:38 +0530

    update

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string; // add your custom env vars here
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
